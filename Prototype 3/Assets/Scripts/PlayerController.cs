732ed35dd597bdb62c3c85b0ab3c67f41084b7e8
Robin Suurs <17059143@student.hhs.nl>
2023-09-13 11:37:47 +0200

    Stap 3 done

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Rigidbody _playerRb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRb = GetComponent<Rigidbody>();
+        _playerRb.AddForce(Vector3.up*1000);
     }
 
     // Update is called once per frame
