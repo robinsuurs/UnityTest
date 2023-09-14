f844fc778eb6b352738be5164c85206ec20bb1bd
Robin Suurs <17059143@student.hhs.nl>
2023-09-14 13:45:34 +0200

    Added Jump

--- Lesson 2/Assets/Script/PlayerControl.cs
+++ Lesson 2/Assets/Script/PlayerControl.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerControl : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnJump(InputValue inputValue)
     {
-        
+        _rigidbody.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
     }
 }
