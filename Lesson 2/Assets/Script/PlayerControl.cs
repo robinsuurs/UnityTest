5a1bfdd709f76ee359633d1aaa78188476d311c7
Robin Suurs <17059143@student.hhs.nl>
2023-09-14 14:12:30 +0200

    Added movement

--- Lesson 2/Assets/Script/PlayerControl.cs
+++ Lesson 2/Assets/Script/PlayerControl.cs
@@ -1,16 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+
+    [SerializeField] private float speed = 10;
+
+    private Vector2 _movement;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(_movement.x ,0,_movement.y) * (speed * Time.deltaTime));
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        _movement = inputValue.Get<Vector2>();
     }
 
     void OnJump(InputValue inputValue)
